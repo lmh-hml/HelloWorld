8da974dbfb1673bc1d03dcd5f7daac295885849b
lmh-hml <mhmail129@gmail.com>
2016-08-25 14:20:01 +0800

    Update Control.h

--- Control.h
+++ Control.h
@@ -19,11 +19,19 @@ class Control
 {
 	public:
 		
-		Control();
-	
-	//The robot values:
-	//Updated through subscription
-		float joy_lx, joy_az;
+	Control();
+
+	//The functions:
+        void Update();
+        MoveState getDirection(float joy_lx, float joy_az);
+        float calc_linearVel(int16_t& sensor_rd, const int& MAX_LIMIT, const int& MIN_LIMIT);
+    	bool sidesBlocked( int16_t right, int16_t left);
+        void dynamicReconfig(fyp::ControlDistConfig &config, uint32_t level );
+        void publishTwist(float linearX, float joylinear ,float 	angularZ, float joyAngular);
+        
+    private:
+    		
+    	float joy_lx, joy_az;
         int16_t sensor[6]={0,0,0,0,0,0};
 	//Updated by local function
         float linear,linearBack, angular;
@@ -34,20 +42,11 @@ class Control
         int MIN_DIST, MAX_DIST,MIN_SIDES;
 		
 	//The ros stuff:		
-		ros::NodeHandle nh;
-		ros::Publisher  Twist_pub;
-		ros::Subscriber Joy_listener;
-		ros::Subscriber US_listener;
+	ros::NodeHandle nh;
+	ros::Publisher  Twist_pub;
+	ros::Subscriber Joy_listener;
+	ros::Subscriber US_listener;
         dynamic_reconfigure::Server<fyp::ControlDistConfig> server;
-
-	//The functions:
-        void Update();
-        MoveState getDirection(float joy_lx, float joy_az);
-        float calc_linearVel(int16_t& sensor_rd, const int& MAX_LIMIT, const int& MIN_LIMIT);
-    	bool sidesBlocked( int16_t right, int16_t left);
-        void dynamicReconfig(fyp::ControlDistConfig &config, uint32_t level );
-        void publishTwist(float linearX, float joylinear ,float 	angularZ, float joyAngular);
-    private:
 
 		void JoyCallBack(const sensor_msgs::Joy::ConstPtr& joy);
 		void SonicCallBack( const fyp::Int16Array::ConstPtr& ary);
